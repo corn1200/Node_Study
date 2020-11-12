c88afc1f01f71447bbb14e14db2eb3f065b3a4df
corn1200 <corn120033@gmail.com>
2020-11-12 21:03:12 +0900

    댓글 테이블

--- 11_12/models/post.js
+++ 11_12/models/post.js
@@ -1,14 +1,24 @@
 const { Schema, model } = require('mongoose')
 
-const userSchema = Schema({
+const commentSchema = Schema({
+    content: { type: String, required: true },
+    writer: { type: String, required: true },
+    createAt: { type: Date, required: true, default: () => new Date() }
+})
+
+const postSchema = Schema({
     title: { type: String, required: true},
     content: { type: String, required: true},
     writer: { type: String, required: true},
-    comment: { type: String, default: false},
-    createAt: { type: Number, required: true},
-    updateAt: { type: Number, default: false}
+    comment: [{
+        type: String,
+        required: true
+    }],
+    createAt: { type: Date, required: true, default: () => new Date() },
+    updateAt: { type: Date, required: true, default: () => new Date() },
+    hit: { type: Number, required: true, default: 0}
 })
 
-const Post = model('post', userSchema)
+const Post = model('post', postSchema)
 
 module.exports = Post
