d7be8818d796141415731ea9f5f03a5309dcf1ca
corn1200 <corn120033@gmail.com>
2020-11-26 20:51:20 +0900

    11월 26일자 수업내용

---
+++ 11_26/index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const crypto = require('crypto')
+const session = require('express-session')
+
+require("./lib/mongoose")
+const User = require('./models/user')
+const Post = require('./models/post')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.static('./11_26/public'))
+app.use((req, res, next) => {
+  console.log('req.path : ', req.path)
+  next()
+})
+app.set('view engine', 'ejs')
+app.set('views', `${__dirname}/views`)
+app.use(session({
+  secret: 'ejs',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.get('/', (req, res) => {
+    console.log(req.session.user)
+    res.render('index', { user: req.session.user })
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/posts', async (req, res) => {
+  const posts = await Post.find({ })
+  res.render('posts', { posts })
+})
+
+app.get('/posts/create', (req, res) => {
+  if(!req.session.user) return res.redirect('/login')
+  res.render('createPost')
+})
+
+app.post('/posts', (req, res) => {
+  if(!req.session.user) return res.redirect('/login')
+  const { body: { title, content } } = req
+  Post.create({ title, content, writer: req.session.user._id })
+  return res.redirect('/posts')
+})
+
+app.post('/login', async function (req, res) {
+  const { body: { id, pw} } = req
+
+  const EPW = crypto.createHash('sha512').update(id + 'd!6b&^a' + pw).digest('base64')
+
+  const data = await User.findOne({id, pw: EPW})
+
+  if (data) {
+    req.session.user = data
+    return res.redirect('/')
+  }
+
+  res.send(`login failed`)
+})
+
+app.post('/registry', function (req, res) {
+  const { body: { id, pw, name } } = req
+
+  const EPW = crypto.createHash('sha512').update(id + 'd!6b&^a' + pw).digest('base64')
+  User.create({ id, pw: EPW, name })
+
+  res.redirect('/')
+})
+
+app.get('/users', async (req, res) => {
+  const data = await User.find({}, { pw: 0})
+  res.json(data)
+})
+
+app.get('/logout', function (req, res) {
+  delete req.session.user
+  res.redirect('/')
+})
+
+const port = 8005
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`)
+})
